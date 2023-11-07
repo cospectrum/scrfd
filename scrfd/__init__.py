2e9b8fd965202c36cf3bcddd901bcb69d0e8bbe5
Alex Severin <severinalexeyv@gmail.com>
2023-11-08 02:19:02 +0300

    backbone

---
+++ scrfd/__init__.py
@@ -0,0 +1,8 @@
+from .schemas import (
+    Face as Face,
+    FaceKeypoints as FaceKeypoints,
+    Bbox as Bbox,
+    Point as Point,
+    Threshold as Threshold,
+)
+from .pub import SCRFD as SCRFD
